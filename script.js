e15f075af0dfd8efa3a01572478ce4c2b09bfc4f
MuhammadIbrar21 <114849910+MuhammadIbrar21@users.noreply.github.com>
2024-03-10 23:55:08 +0500

    change UI and add scale details on left side

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
-let dollarRate = [160, 180, 240, 160, 280]
+let dollarRate = [
+    { currencyRate: 160, month: "jan" },
+    { currencyRate: 180, month: "feb" },
+    { currencyRate: 240, month: "mar" },
+    { currencyRate: 160, month: "apr" },
+    { currencyRate: 280, month: "may" },
+    { currencyRate: 300, month: "jun" },
+]
 
 generateBar()
 generateBarDetails()
@@ -9,16 +16,16 @@ function generateBar() {
 
     let maxBarHeight = barContainer.offsetHeight - 10;
 
+    let max = maximium(dollarRate);
+
+    let height;
+
     for (let rate of dollarRate) {
-
-        let max = Math.max(...dollarRate);
-
-        let height;
 
         if (rate == max) {
             height = maxBarHeight;
         } else {
-            let rateP = rate * 100 / max;
+            let rateP = rate.currencyRate * 100 / max;
             height = rateP * maxBarHeight / 100;
         }
 
@@ -39,10 +46,35 @@ function generateBar() {
 
 function generateBarDetails() {
     let barDetails = document.getElementById('bar-details');
+    let scaleValue = document.getElementById('left-scale');
+
+    let max = maximium(dollarRate);
+
+    let iterated;
+    let totalScale;
+    if (max <= 300) {
+        iterated = 50;
+        totalScale = 7;
+    } else if (max > 300 && max <= 400) {
+        iterated = 60;
+        totalScale = 8;
+    } else if (max > 400 && max <= 500) {
+        iterated = 75;
+        totalScale = 8;
+    }
+    for (let i = 0; i < totalScale; i++) {
+        scaleValue.innerHTML += `
+            <div>${i * iterated}</div>
+      `
+    }
+
+    dollarRate.sort((n, p) => {
+        return n.month - p.month;
+    })
 
     for (let detail of dollarRate) {
         barDetails.innerHTML += `
-                <div>${detail}</div>
+                <div>${detail.month}</div>
         `
     }
 }
@@ -53,13 +85,36 @@ function diffColor() {
     return colors[Math.floor(Math.random() * colors.length)]
 }
 
+function maximium(arr) {
+    let maxValue = Number.MIN_VALUE;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].currencyRate > maxValue) {
+            maxValue = arr[i].currencyRate;
+        }
+    }
+    return maxValue;
+}
+
+function randomMonth() {
+    let monthList = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+    return monthList[Math.floor(Math.random() * monthList.length)];
+};
+
 let increment = document.getElementById('incre');
 let decrement = document.getElementById('decre');
 
 increment.addEventListener('click', () => {
+    let max = maximium(dollarRate);
+    let currencyRate = Math.floor(Math.random() * max)
+    let montName = randomMonth();
     if (dollarRate.length < 24) {
-        dollarRate.push(Math.floor(Math.random() * Math.max(...dollarRate)));
+        let newObj = {
+            currencyRate: currencyRate,
+            month: montName,
+        }
+        dollarRate.push(newObj);
     }
+    console.log(max);
     if (dollarRate.length == 23) {
         increment.disabled = true;
     } else if (dollarRate.length > 1) {
@@ -67,9 +122,11 @@ increment.addEventListener('click', () => {
     }
     let barContainer = document.getElementById('bars');
     let barDetails = document.getElementById('bar-details');
+    let scaleValue = document.getElementById('left-scale');
 
     barContainer.innerHTML = ''
     barDetails.innerHTML = ''
+    scaleValue.innerHTML = ''
 
     generateBar()
     generateBarDetails()
@@ -85,9 +142,11 @@ decrement.addEventListener('click', () => {
     }
     let barContainer = document.getElementById('bars');
     let barDetails = document.getElementById('bar-details');
+    let scaleValue = document.getElementById('left-scale');
 
     barContainer.innerHTML = ''
     barDetails.innerHTML = ''
+    scaleValue.innerHTML = ''
 
     generateBar()
     generateBarDetails()
